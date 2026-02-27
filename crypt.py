831faef850f5b1b6f9b85cf053085787d8bf3f97
FaseehFS <faseeh1080@gmail.com>
2026-02-27 11:53:04 +0530

    Use menubar

--- crypt.py
+++ crypt.py
@@ -1,37 +1,41 @@
 import os
 from cryptography.fernet import Fernet
 
+
 def generate_key_and_cipher_suite():
     key = Fernet.generate_key()
     cipher_suite = Fernet(key)
     return key, cipher_suite
 
+
 def create_key_file_and_write_key_to_it(path_to_key_file, key):
-    with open(path_to_key_file, 'wb') as key_file:
+    with open(path_to_key_file, "wb") as key_file:
         key_file.write(key)
 
+
 def encrypt_file(path_to_file, cipher_suite):
-    with open(path_to_file, 'rb') as the_file:
+    with open(path_to_file, "rb") as the_file:
         content = the_file.read()
-    with open(path_to_file, 'wb') as the_file:
+    with open(path_to_file, "wb") as the_file:
         content_encrypted = cipher_suite.encrypt(content)
         the_file.write(content_encrypted)
+
 
 def add_extension(path_to_file, extension):
     new_path = path_to_file + extension
     os.rename(path_to_file, new_path)
 
 
-
 def read_key_and_create_cipher_suite(path_to_key_file):
-    with open(path_to_key_file, 'rb') as key_file:
+    with open(path_to_key_file, "rb") as key_file:
         key = key_file.read()
     cipher_suite = Fernet(key)
     return key, cipher_suite
 
+
 def decrypt_file(path_to_file, cipher_suite):
-    with open(path_to_file, 'rb') as the_file:
+    with open(path_to_file, "rb") as the_file:
         content = the_file.read()
-    with open(path_to_file, 'wb') as the_file:
+    with open(path_to_file, "wb") as the_file:
         content_decrypted = cipher_suite.decrypt(content)
         the_file.write(content_decrypted)
--- main.py
+++ main.py
@@ -3,28 +3,29 @@ from tkinter import font
 from tkinter import filedialog
 import os
 from cryptography.fernet import Fernet
-from functions import *
+from crypt import *
 
 root = tk.Tk()
-root.title("File Encrypter by Faseeh")
+root.title("File Encrypter")
 root.resizable(False, False)
 
-# Main variables:
 enc = "Encrypt"
 dec = "Decrypt"
 no_action = "-"
-action = no_action  # Shows at the action button and used to decide whether we need to encrypt or decrypt files.
-info = "No file selected."  # Shown at the right of the screen.
+action = no_action
+info = "No file selected."
 filepath = None
 
-ENCRYPTED_FILE_EXTENSION = '.enc'
+ENCRYPTED_FILE_EXTENSION = ".enc"
 
-# Functions:
+
 def refresh_action_btn():
     action_btn.config(text=action)
 
+
 def refresh_info_l():
     info_l.config(text=info)
+
 
 def file_e_func():
     global filepath, action, info
@@ -44,6 +45,7 @@ def file_e_func():
     refresh_action_btn()
     refresh_info_l()
 
+
 def action_func():
     global filepath, action
     if filepath is None or action == no_action:
@@ -53,7 +55,7 @@ def action_func():
     try:
         if action == enc:
             key, cipher_suite = generate_key_and_cipher_suite()
-            path_to_key_file = filepath + '.key'
+            path_to_key_file = filepath + ".key"
             create_key_file_and_write_key_to_it(path_to_key_file, key)
             encrypt_file(filepath, cipher_suite)
             add_extension(filepath, ENCRYPTED_FILE_EXTENSION)
@@ -63,11 +65,11 @@ def action_func():
             info = f"{filename} has been encrypted."
             refresh_info_l()
         elif action == dec:
-            path_to_key_file = filepath[:-4] + '.key'
+            path_to_key_file = filepath[:-4] + ".key"
             key, cipher_suite = read_key_and_create_cipher_suite(path_to_key_file)
             decrypt_file(filepath, cipher_suite)
-            os.rename(filepath, filepath[:-4])  # To remove the .enc extension.
-            filepath = filepath[:-4]  # To remove the .enc extension.
+            os.rename(filepath, filepath[:-4])
+            filepath = filepath[:-4]
             os.remove(path_to_key_file)
             action = enc
             refresh_action_btn()
@@ -77,14 +79,14 @@ def action_func():
         info = str(e)
         refresh_info_l()
 
-# UI variables
+
 scale_var = 1.0
 pad1 = 4 * scale_var
 pad2 = 2 * scale_var
 
-# Font settings
 custom_font = font.Font(size=int(10 * scale_var))
 custom_font2 = font.Font(size=int(8 * scale_var))
+
 
 def update_widgets():
     custom_font.config(size=int(10 * scale_var))
@@ -93,13 +95,10 @@ def update_widgets():
     border_size = max(2, int(20 * scale_var) / 10)
     action_btn.config(font=custom_font, borderwidth=border_size)
     file_e.config(font=custom_font, borderwidth=border_size)
-    
-    height = 2
-    scale_down.config(font=custom_font2, height=height)
-    scale_up.config(font=custom_font2, height=height)
-    help_button.config(font=custom_font2, height=height)
 
+    height = 2
     info_l.config(font=custom_font)
+
 
 def scale_text(increment):
     global scale_var
@@ -107,17 +106,20 @@ def scale_text(increment):
     scale_var = max(1, min(scale_var, 5))
     update_widgets()
 
-# Widgets:
-menu = tk.Frame(root, bd=2, relief=tk.RAISED, padx=pad2)
-menu.grid(row=0, column=0, columnspan=3, sticky='ew')
 
-menu_font = font.Font(size=8)
-scale_down = tk.Button(menu, text="<<", relief=tk.FLAT, font=menu_font, command=lambda: scale_text(-0.25))
-scale_down.grid(row=0, column=0, sticky='nsew')
-scale_up = tk.Button(menu, text=">>", relief=tk.FLAT, font=menu_font, command=lambda: scale_text(0.25))
-scale_up.grid(row=0, column=1, sticky='nsew')
-help_button = tk.Button(menu, text="Help", relief=tk.FLAT, font=menu_font)
-help_button.grid(row=0, column=2, sticky='nsew')
+menu_bar = tk.Menu(root)
+
+view_menu = tk.Menu(menu_bar, tearoff=0)
+view_menu.add_command(label="Zoom In", command=lambda: scale_text(0.25))
+view_menu.add_command(label="Zoom Out", command=lambda: scale_text(-0.25))
+
+help_menu = tk.Menu(menu_bar, tearoff=0)
+help_menu.add_command(label="Help")
+
+menu_bar.add_cascade(label="View", menu=view_menu)
+menu_bar.add_cascade(label="Help", menu=help_menu)
+
+root.config(menu=menu_bar)
 
 root.grid_columnconfigure(0, weight=1)
 root.grid_columnconfigure(1, weight=1)
@@ -126,21 +128,28 @@ root.grid_rowconfigure(1, weight=1)
 root.grid_rowconfigure(2, weight=1)
 
 sel_f = tk.Label(root, text="Select the file to encrypt:", font=custom_font)
-sel_f.grid(row=1, column=0, padx=pad1, pady=pad1, sticky='nsew')
-file_e = tk.Button(root, text="File Explorer", padx=pad1, pady=pad1, command=file_e_func)
-file_e.grid(row=1, column=1, padx=pad1, pady=pad1, sticky='nsew')
+sel_f.grid(row=1, column=0, padx=pad1, pady=pad1, sticky="nsew")
+
+file_e = tk.Button(
+    root, text="File Explorer", padx=pad1, pady=pad1, command=file_e_func
+)
+file_e.grid(row=1, column=1, padx=pad1, pady=pad1, sticky="nsew")
 
 info_l = tk.Label(root, text=info, font=custom_font, wraplength=0)
-info_l.grid(row=1, column=2, rowspan=2, padx=pad1, pady=pad1, sticky='nsew')
+info_l.grid(row=1, column=2, rowspan=2, padx=pad1, pady=pad1, sticky="nsew")
+
 
 def update_info_label():
     current_width = (root.winfo_width() - (2 * pad1)) / 2.3
     info_l.config(wraplength=current_width)
 
+
 root.bind("<Configure>", lambda event: update_info_label())
 update_info_label()
 
-action_btn = tk.Button(root, text=action, padx=pad1, pady=pad1, font=custom_font, command=action_func)
-action_btn.grid(row=2, column=0, columnspan=2, padx=pad1, pady=pad1, sticky='nsew')
+action_btn = tk.Button(
+    root, text=action, padx=pad1, pady=pad1, font=custom_font, command=action_func
+)
+action_btn.grid(row=2, column=0, columnspan=2, padx=pad1, pady=pad1, sticky="nsew")
 
 root.mainloop()
